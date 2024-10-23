2c54d04c0c740e489039a6a16bc5160469955c74
VittorioCiboddo <vittorio.ciboddo.vc95@gmail.com>
2024-10-23 10:59:47 +0200

    aggiunta progetto IntelliJ

---
+++ CODE/project-work_giornale/src/main/java/com/example/project_work_giornale/ProjectWorkGiornaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_work_giornale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWorkGiornaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWorkGiornaleApplication.class, args);
+	}
+
+}
---
+++ CODE/project-work_giornale/src/test/java/com/example/project_work_giornale/ProjectWorkGiornaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_work_giornale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWorkGiornaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
