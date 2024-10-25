262cbd6f15593fd98228b23fb509eb58abf86d41
VittorioCiboddo <vittorio.ciboddo.vc95@gmail.com>
2024-10-25 17:06:37 +0200

    Implementazione del login admin (controlli con db OK, proposta di grafica)

--- CODE/IntelliJ/project-work_giornale/src/main/java/com/example/project_work_giornale/controller/LoginAdminController.java
+++ CODE/IntelliJ/project-work_giornale/src/main/java/com/example/project_work_giornale/controller/LoginAdminController.java
@@ -4,10 +4,13 @@ import com.example.project_work_giornale.service.AdminService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+// localhost:8080/loginadmin
 @Controller
 @RequestMapping("/loginadmin")
 public class LoginAdminController {
@@ -15,13 +18,22 @@ public class LoginAdminController {
     @Autowired
     private AdminService adminService;
 
+    @GetMapping
+    public String getPage(@RequestParam(name = "error", required = false) String error, Model model, HttpSession session) {
+        if (session.getAttribute("utente") != null)
+            return "redirect:/areaadmin";
+        model.addAttribute("error", error != null);
+        return "login-admin";
+    }
+
+
     @PostMapping
     public String formManager(
             @RequestParam("username") String username,
             @RequestParam("password") String password,
             HttpSession session) {
         if(!adminService.loginAdmin(username, password, session))
-            return "redirect:/logiadmin?error";
-        return "redirect:/riservataadmin";
+            return "redirect:/loginadmin?error";
+        return "redirect:/areaadmin";
     }
 }
