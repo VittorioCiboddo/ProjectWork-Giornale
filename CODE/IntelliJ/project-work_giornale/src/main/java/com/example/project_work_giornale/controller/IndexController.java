ddbae06cf1e1233458a3dfd051f6a2416cc862fd
Reix <reixwolf2004@gmail.com>
2024-10-25 10:05:51 +0200

    branch commit notizia

--- CODE/IntelliJ/project-work_giornale/src/main/java/com/example/project_work_giornale/controller/IndexController.java
+++ CODE/IntelliJ/project-work_giornale/src/main/java/com/example/project_work_giornale/controller/IndexController.java
@@ -1,6 +1,8 @@
 package com.example.project_work_giornale.controller;
 
+import com.example.project_work_giornale.model.Notizia;
 import com.example.project_work_giornale.service.AdminService;
+import com.example.project_work_giornale.service.NotiziaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,8 +16,13 @@ import java.util.List;
 @RequestMapping("/")
 public class IndexController {
 
+    //@Autowired
+    private NotiziaService notiziaService;
+
     @GetMapping
-    public String getPage() {
+    public String getPage(Model model) {
+        List<Notizia> notizia = notiziaService.elencoNotizia();
+        model.addAttribute("notizia", notizia);
         return "index";
     }
 
